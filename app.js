ea59fdad176ae0591180e54d84efa08650a997a1
wisnudj <jayalahwisnu@gmail.com>
2017-11-07 16:33:29 +0700

    bikin app dan ejs

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const app = express()
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.set('views', './views') // specify the views directory
+app.set('view engine', 'ejs') // register the template engine
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
